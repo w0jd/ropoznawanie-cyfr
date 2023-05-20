5082c944f6c2c397d76af490d602d5ac612d90da
w0jd <dwoitek1@gmail.com>
2023-05-20 22:31:21 +0200

    normalizacja + podpisanie danych u góry

--- dane.py
+++ dane.py
@@ -18,8 +18,24 @@ splaszczoneTesty = []
 splaszczone2 = []
 norm = []
 stra = ""
+for i in range(28):
+    for j in range(28):
+        print("dupa")
+        plik.write('"')
+
+        plik.write(str(i)+str(j))
+        plik.write('"')
+        plik.write(",")
+plik.write('"')
+
+plik.write("label")
+plik.write('"')
+
+plik.write("\n")
+
 for i in range(ile):
     # splaszczone2.append(np.ravel(x_train[i]))
+
     stra = str(np.ravel(x_train[i]))
     stra = stra.replace('\n', ' ')
 
@@ -34,9 +50,10 @@ for i in range(ile):
     stra = stra[1:]
     for let in stra:
         if let == "\n":
-            plik.write(",'")
+            plik.write(",")
+            plik.write('"')
             plik.write(str(y_train[i]))
-            plik.write("'")
+            plik.write('"')
 
         plik.write(let)
     # plik.write("\n")
@@ -57,71 +74,18 @@ for i in range(ile2):
     stra = stra[1:]
     for let in stra:
         if let == "\n":
-            plik2.write(",'")
+            plik2.write(",")
+            plik2.write('"')
             plik2.write(str(y_test[i]))
-            plik2.write("'")
+            plik2.write('"')
         plik2.write(let)
 
-    # plik2.write(",'")
-    # plik2.write(x_test[i])
-    # plik2.write("'")
+    plik2.write(",'")
+    plik2.write(x_test[i])
+    plik2.write("'")
 # plt.imshow(x_train[0], cmap='gray')
 # print (y_train[0])
 # splaszczone2 = list(map(int, splaszczone2))
 
 # plt.axis('off')
 # plt.show()
-
-
-def maxImin(list, pocz, kon):
-    # list = rd.sample(list1, len(list1))
-    maxlist = []
-    minList = []
-    for i in range(kon-1):
-        zmienna = int(list[i][0])
-        if isinstance(zmienna, (int, float)):
-            maxlist.append(zmienna)
-            minList.append(zmienna)
-
-    for i in range(len(list)):
-        for j in range(kon-1):
-            zmienna = int(list[i][j])
-            dl = len(maxlist[i])
-            maks = int(maxlist[j][dl])
-            mins = int(minList[j][dl])
-            if isinstance(zmienna, (int, float)):
-
-                if zmienna > maks:
-                    maxlist[j][-1] = zmienna
-                if zmienna < mins:
-                    minList[j][-1] = zmienna
-    # print(minList)
-    # print(maxlist)
-
-    for i in range(len(list)):
-        for j in range(kon-1):
-            zmienna = int(list[i][j])
-            mins = int(maxlist[j][dl])
-            mins = int(minList[j][dl])
-            if isinstance(zmienna, (int, float)):
-                list[i][j] = (zmienna - mins) / \
-                    (mins - mins)
-        # print(list)
-    return (list)
-
-
-tabOdl = []
-# print(len(splaszczone2[0]))
-# for i in range(len(splaszczone2)):
-#     for j in range(len(splaszczone2[0])):
-#         tabOdl.append(int(splaszczone2[i][j]))
-# print(tabOdl)
-
-
-def tasowanie(list1):
-    list = rd.sample(list1, len(list1))
-    return (list)
-
-
-# norm = maxImin(splaszczone2, 1, len(splaszczone2[0]))
-# print(norm[0])
